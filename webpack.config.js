9b00a71f726844d315e8ca3dc71440265c885cae
olivier <olivier.denis@hotmail.be>
2021-06-19 13:29:06 +0200

    base workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,145 +1,52 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const postcssPresetEnv = require('postcss-preset-env');
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  devtool: 'source-map',
   entry: {
-    app: ['@babel/polyfill', './src/main.js'],
+    main: path.join(__dirname, "src/index.js"),
   },
-  mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].js',
+    path: path.join(__dirname, "dist"),
+    filename: "[name].bundle.js",
   },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    open: true,
-  },
-  performance: {
-    maxEntrypointSize: 1000000,
-    maxAssetSize: 1000000,
-    hints: 'warning',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'My App',
-      template: './src/index.html',
-      filename: 'index.html',
-      minify: false,
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-      chunkFilename: '[id].css',
-    }),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.(sa|sc|c)ss$/,
-      cssProcessor: require('cssnano'),
-      cssProcessorPluginOptions: {
-        preset: [
-          'default',
-          {
-            discardComments: {
-              removeAll: true,
-            },
-          },
-        ],
-      },
-      canPrint: true,
-    }),
-  ],
   module: {
     rules: [
       {
-        test: /^(?!.*\.{test,min}\.js$).*\.js$/,
+        test: /\.js/,
         exclude: /(node_modules)/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        }],
+        use: ["babel-loader"],
       },
       {
-        test: /\.(sa|sc|c)ss$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: MiniCssExtractPlugin.loader,
-          options: {
-            publicPath: '../',
-          },
-        },
-        {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
-            importLoaders: 2,
-          },
-        },
-        {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true,
-            importLoaders: 1,
-          },
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            ident: 'postcss',
-            plugins: () => [
-              postcssPresetEnv({
-                browsers: 'last 2 versions',
-                autoprefixer: {
-                  grid: true,
-                  cascade: false,
-                },
-                stage: 3,
-                features: {
-                  'nesting-rules': true,
-                },
-              }),
-            ],
-          },
-        },
-        ],
-      },
-      {
-        test: /\.pug$/,
-        use: 'pug-loader',
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'img/',
-          },
-        }],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts/',
-          },
-        }],
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-        options: {
-          minimize: true,
-        },
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./src/assets/images/*",
+          to: "assets/images/[name][ext]",
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "./src/index.html"),
+      chunks: ["main", "topbar"],
+    }),
+  ],
+  stats: "minimal",
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    open: false,
+    contentBase: path.resolve(__dirname, "./dist"),
+    inline: true,
+    port: 4000,
   },
 };
